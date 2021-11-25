3a7978178c3aff8ad632a3f0a64948df9bdb49c5
Samir <46187856+secdevoops@users.noreply.github.com>
2021-11-25 14:31:20 +0100

    Update startup.sh

--- docker_files/startup.sh
+++ docker_files/startup.sh
@@ -2,12 +2,10 @@
 
 CMD="/usr/local/bin/docker-entrypoint.sh mysqld"
 
-if  [ -n "$GALERA_NEW_CLUSTER" ]; then
-  if [ ! -e "/var/lib/mysql/mysql" ];  then
+if  [ -n "$GALERA_NEW_CLUSTER" ] && [ ! -e "/var/lib/mysql/mysql" ];  then
     CMD="$CMD --wsrep-new-cluster"
-  elif [ ! -e "/var/lib/mysql/gvwstate.dat" ] && [ -e "/var/lib/mysql/grastate.dat" ] &&  $(grep -q "safe_to_bootstrap: 1" /var/lib/mysql/grastate.dat) ; then
+elif [ ! -e "/var/lib/mysql/gvwstate.dat" ] && [ -e "/var/lib/mysql/grastate.dat" ] &&  $(grep -q "safe_to_bootstrap: 1" /var/lib/mysql/grastate.dat) ; then
     CMD="$CMD --wsrep-new-cluster"
-  fi
 fi
 
 exec $CMD
